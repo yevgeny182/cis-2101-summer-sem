8120760fc6383f0b0988c08d2b2f18188d3e1f4e
Yev Albano <94223536+yevgeny182@users.noreply.github.com>
2024-06-15 00:47:26 +0800

    main.c with codes
    added codes in main for stack linked list

--- Stack-LL/main.c
+++ Stack-LL/main.c
@@ -3,5 +3,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	return 0;
+    StackLinkedList mainStack = createStack();
+    stack_push(&mainStack, 22);
+    stack_push(&mainStack, 56);
+    stack_push(&mainStack, 99);
+    visualize(mainStack);
+    
+    
+    stack_pop(&mainStack);
+    visualize(mainStack);
+    
+    int peekValue = stack_peek(mainStack);
+    printf("%d", peekValue);
 }
