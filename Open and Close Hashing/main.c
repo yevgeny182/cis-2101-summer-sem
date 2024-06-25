7e6e7769a3e248341f1399b87f278a3719696614
Yev Albano <94223536+yevgeny182@users.noreply.github.com>
2024-06-25 15:33:05 +0800

    Add files via upload
    Renamed stuff

--- Open and Close Hashing/main.c
+++ Open and Close Hashing/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "OpenandClose.h"
+#include "OpenHash.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
